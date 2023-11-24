e9a6825e4b005a44511e72da5025547b0a923816
dparedesh <danielakparedes@gmail.com>
2023-11-24 15:22:44 +0100

    fixing linting

--- clean_tuberculosis.py
+++ clean_tuberculosis.py
@@ -1,28 +1,45 @@
+""" Clean tuberculosis data
+
+This script is intended to clean the tuberculosis dataset, dedicated
+mainly to rename the columns of the original dataset DATA_FILE using the
+dictionary provided VAR_FILE
+"""
+
 import pandas as pd
 
+DATA_FILE = "data/Tuberculosis/TB_burden_countries_2023-11-22.csv"
 
-data_file = "data/Tuberculosis/TB_burden_countries_2023-11-22.csv"
+VAR_FILE = "data/Tuberculosis/TB_data_dictionary_2023-11-22.csv"
 
-var_file = "data/Tuberculosis/TB_data_dictionary_2023-11-22.csv"
-
-
-data = pd.read_csv(data_file)
-
-var = pd.read_csv(var_file)
-
-data_cols = list(data_df.columns)
-
-var_names = {'year':'year'}
-
-for col in data_cols:
-
-	if col == 'year': continue
-	var_name = var_names[var_names['variable_name'] == col]['definition']
-	var_names[col] = var_name.iat[0]
+CLEANED_FILE = "data/Tuberculosis/tuberculosis.csv"
 
 
-data.rename(columns = var_names,inplace=True)
+def main():
 
-data.to_csv("data/tuberculosis/tuberculosis_2022.csv")
+    """
+    Rename variables of DATA_FILE using the names from VAR_FILE
+    File with renamed variables is written in CLEANED_FILE.
+    """
+
+    data = pd.read_csv(DATA_FILE)
+
+    var = pd.read_csv(VAR_FILE)
+
+    data_names = list(data.columns)
+
+    var_names = {'year': 'year'}
+
+    for col in data_names:
+
+        if col == 'year':
+            continue
+        var_name = var[var['variable_name'] == col]['definition']
+        var_names[col] = var_name.iat[0]
+
+    data.rename(columns=var_names, inplace=True)
+
+    data.to_csv(CLEANED_FILE)
 
 
+if __name__ == "__main__":
+    main()
